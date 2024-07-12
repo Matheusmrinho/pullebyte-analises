820b724fe1ac295aa462efe4868a4c34f25ce136
Luis Filipe <135190249+LuisFilipeOliveiraSantos@users.noreply.github.com>
2024-07-11 21:19:34 -0300

    merge clubs + keystats e começo para analises

---
+++ DataSet Project/Merge Clubs + KeyStats.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import unidecode
+from fuzzywuzzy import process, fuzz
+import time
+
+start_time = time.time()
+df_players = pd.read_parquet(r'Caminho para o arquivo clubs.parquet')
+df_keystats = pd.read_parquet(r'Caminho para o arquivo keystats.parquet')
+print(f"Tempo de carregamento dos dados: {time.time() - start_time:.2f} segundos")
+
+df_players['name'] = df_players['name'].str.lower().str.strip().apply(unidecode.unidecode)
+if 'club' in df_keystats.columns:
+    df_keystats['club'] = df_keystats['club'].str.lower().str.strip().apply(unidecode.unidecode)
+    merge_key = 'club'
+else:
+    raise KeyError("Coluna 'club' não encontrada em df_keystats")
+
+min_similarity = 75
+
+def fuzzy_match(name, choices):
+    match = process.extractOne(name, choices, scorer=fuzz.token_set_ratio)
+    return match[0] if match and match[1] >= min_similarity else None
+
+df_players['matched_name'] = df_players['name'].apply(lambda x: fuzzy_match(x, df_keystats[merge_key]))
+df_players.dropna(subset=['matched_name'], inplace=True)
+
+df_combined = pd.merge(df_keystats, df_players, left_on=merge_key, right_on='matched_name', how='left').drop_duplicates(subset=['player_name'])
+
+output_path = r'Local para salvamento do merge (caminho / merge.parquet)'
+df_combined.fillna(0).to_parquet(output_path, index=False)
+
